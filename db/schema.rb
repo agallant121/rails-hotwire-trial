78edb04d614104a1268183cf6585b885fdfafe73
Alex Gallant <agallant1201@gmail.com>
2026-05-07 12:57:01 -0400

    Add Like model and migration for user-photo relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_05_07_142510) do
+ActiveRecord::Schema[8.0].define(version: 2026_05_07_153600) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "photo_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["photo_id"], name: "index_likes_on_photo_id"
+    t.index ["user_id", "photo_id"], name: "index_likes_on_user_id_and_photo_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "photos", force: :cascade do |t|
     t.integer "pexels_id", null: false
     t.integer "width", null: false
@@ -36,4 +46,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_05_07_142510) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "likes", "photos"
+  add_foreign_key "likes", "users"
 end
