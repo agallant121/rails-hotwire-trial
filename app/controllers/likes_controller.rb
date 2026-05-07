836b2274e44b5a912d58bafe0638da29dc5c84f6
Alex Gallant <agallant1201@gmail.com>
2026-05-07 13:14:46 -0400

    Add LikesController for managing photo likes with create and destroy actions

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,28 @@
+class LikesController < ApplicationController
+  before_action :set_photo
+
+  def create
+    current_user.likes.find_or_create_by!(photo: @photo)
+    respond_with_like_button
+  end
+
+  def destroy
+    current_user.likes.find_by(photo: @photo)&.destroy!
+    respond_with_like_button
+  end
+
+  private
+
+  def set_photo
+    @photo = Photo.find(params[:photo_id])
+  end
+
+  def respond_with_like_button
+    @photo.reload
+
+    respond_to do |format|
+      format.html { redirect_to photos_path }
+      format.turbo_stream
+    end
+  end
+end
