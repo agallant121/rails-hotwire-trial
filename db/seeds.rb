60c07e9bde9be5ae80f759e2806f5cd3f96598c3
Alex Gallant <agallant1201@gmail.com>
2026-05-07 11:16:19 -0400

    Add csv gem for reading seed data from photos.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,19 @@
+require "csv"
+
 User.find_or_create_by!(email: "demo@example.com") do |user|
   user.password = "password"
   user.password_confirmation = "password"
+end
+
+CSV.foreach(Rails.root.join("photos.csv"), headers: true) do |row|
+  photo = Photo.find_or_initialize_by(pexels_id: row.fetch("id"))
+
+  photo.update!(
+    width: row.fetch("width"),
+    height: row.fetch("height"),
+    source_url: row.fetch("url"),
+    photographer: row.fetch("photographer"),
+    medium_url: row.fetch("src.medium"),
+    alt: row["alt"]
+  )
 end
