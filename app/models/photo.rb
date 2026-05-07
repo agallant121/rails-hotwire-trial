b74debd4e3d9644719cbeab9bf6b67cd84f5cec8
Alex Gallant <agallant1201@gmail.com>
2026-05-07 12:57:27 -0400

    Add associations for likes in Photo and User models

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,7 @@
- class Photo < ApplicationRecord
+class Photo < ApplicationRecord
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
+
   validates :pexels_id, :width, :height, :source_url, :photographer, :medium_url, presence: true
   validates :pexels_id, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_photos, through: :likes, source: :photo
 end
