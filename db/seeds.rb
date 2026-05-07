05738b1319ea9b14c943483424b41ca0fa6083f3
Alex Gallant <agallant1201@gmail.com>
2026-05-07 16:28:45 -0400

    Seed demo users with initial likes for photos in the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,15 @@ CSV.foreach(Rails.root.join("photos.csv"), headers: true) do |row|
     alt: row["alt"]
   )
 end
+
+demo_user = User.find_by!(email: "demo@example.com")
+demo_user_two = User.find_by!(email: "demo2@example.com")
+photos = Photo.order(:id).limit(5).to_a
+
+photos.first(4).each do |photo|
+  demo_user.likes.find_or_create_by!(photo: photo)
+end
+
+photos.first(2).append(photos.fifth).each do |photo|
+  demo_user_two.likes.find_or_create_by!(photo: photo)
+end
