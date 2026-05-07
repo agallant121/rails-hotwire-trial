9a8c2d4e2e0f8f012c79d379dc675493ec2a781e
Alex Gallant <agallant1201@gmail.com>
2026-05-07 15:07:22 -0400

    Implement pagination for photo index and optimize liked photo ID retrieval

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ require "set"
 
 class PhotosController < ApplicationController
   def index
-    @photos = Photo.order(:id)
-    @liked_photo_ids = current_user.likes.where(photo: @photos).pluck(:photo_id).to_set
+    @pagy, @photos = pagy(:offset, Photo.order(:id), limit: 20)
+    @liked_photo_ids = current_user.likes.where(photo_id: @photos.select(:id)).pluck(:photo_id).to_set
   end
 end
